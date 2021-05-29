5096c54085ebf3176e6b5ef003a027b2df41f113
Abhinav <abhinavmunagala4@gmail.com>
2021-05-29 20:29:16 +0530

    first events page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,url_for
 from flask_mysqldb import MySQL
+import random
 
 app = Flask(__name__)
 
@@ -10,6 +11,9 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
+
+
+card_colors = ['#abad59','#6f8faf','#9693b2','#deb887','#a14633','#862657','#5b6466','#685a4e','#009440','#fc8eac','#ffa500','#c0c0c0','#4b0082','#ff033e','#009691','#febaad']
 
 @app.route('/careerNews')
 def careerNews():
@@ -22,14 +26,17 @@ def event(id):
 
 @app.route('/home')
 def home():
-    
-    
-    # cur = mysql.connection.cursor()
-    # cur.execute('select * from test')
-    # rows = cur.fetchall()
-    # cur.close()
+    global card_colors
+    colors_len = len(card_colors)-1
 
-    return render_template('home.html',id=str(1))#rows=rows
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM EVENT_NAMES ORDER BY START_DATE DESC')
+    rows = cur.fetchall()
+    cur.close()
+    for i in range(len(rows)):
+        rows[i]['color'] = card_colors[random.randint(1,colors_len)]
+
+    return render_template('home.html',rows=rows)
 
 @app.route('/login')
 def login():
