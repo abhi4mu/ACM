ddb1fa28b7a3e970a1d5cf132d6274f6101a0ae2
JoelNada <66004734+JoelNada@users.noreply.github.com>
2021-05-31 21:53:18 +0530

    Create career_news.sql

---
+++ SQL/career_news.sql
@@ -0,0 +1,12 @@
+insert into CAEEER_NEWS values
+("US IT Jobs Growth Continues With Pandemic in the Rearview Mirror<br>Computerworld,May 12","https://www.computerworld.com/article/3542681/us-it-jobs-growth-continues-with-pandemic-in-the-rearview-mirror.html"),
+("The Battle For Tech Talent Is Heating Up<br>The Business Journals,April 21","https://www.bizjournals.com/bizjournals/news/2021/04/21/tech-talent-top-markets.html"),
+("Everything You Need to Know to Land a Job in Data Science<br>ZDNet,May 4","https://www.zdnet.com/article/everything-you-need-to-know-to-land-a-job-in-data-science/"),
+("Can We Make Coding Interviews Better?<br>Tech Republic,May 11","https://www.techrepublic.com/article/coding-interviews-are-terrible-can-we-make-them-better/"),
+("Robots Will Not Take Jobs If CIOs Start Preparing the Workforce Now<br>CIO Dive,April 13","https://www.ciodive.com/news/MIT-Sloan-CIO-symposium-future-work-automation/598329/"),
+("Candidate Screening and Social Media: What You Need to Know<br>Dice Insights,May 11","https://insights.dice.com/2021/05/11/candidate-screening-and-social-media-what-you-need-to-know/"),
+("How to Spot a Collaboration Superstar in Interviews<br>The Enterprisers Project,May 12","https://enterprisersproject.com/article/2021/5/it-leadership-how-spot-collaboration-superstar-interviews"),
+("How to Quit Your Job in the Great Post-Pandemic Resignation Boom<br>Bloomberg Business week,May 10","https://www.bloomberg.com/news/articles/2021-05-10/quit-your-job-how-to-resign-after-covid-pandemic"),
+("The Artificial Intelligence Era: What Will the Future Look Like?<br>The CCC Blog,May 11","https://cccblog.org/2021/05/11/watch-the-artificial-intelligence-era-what-will-the-future-look-like/"),
+("The 10 Best Practices for Remote Software Engineering<br>Communications of the ACM,May 2021","https://cacm.acm.org/magazines/2021/5/252174-the-10-best-practices-for-remote-software-engineering/fulltext")
+;
