358d6d20285e60816ce42b990212140b4080bbd7
JoelNada <66004734+JoelNada@users.noreply.github.com>
2021-06-01 20:49:03 +0530

    Create student_members.sql

---
+++ SQL/student_members.sql
@@ -0,0 +1,12 @@
+INSERT INTO STUDENT_MEMBERS VALUES 
+(1, "18501A0591","Sai Siddhartha Parvathaneni","",'M',"sai.siddhartha.parvathaneni@gmail.com",""),
+(2, "18501A0593","Siva Venkata Sai Rohith Penugonda","",'M',"princerohith132000@gmail.com",""),
+(3, "18501A0518","Meghana Burra","",'F',"burrameghana@gmail.com",""),
+(4, "18501A0575","Bindu Vardhani Marrapu","",'F',"bindu.marrapu@gmail.com",""),
+(5, "18501A0581","Abhinav Munagala","",'M',"abhinavmunagala4@gmail.com",""),
+(6, "18501A05B9","Yarrapathruni N V Sai Bhargava","",'M',"bhargavaynvs@gmail.com",""),
+(7, "18501A05B6","Venkata Naga Sai Gowthami Cheruvu","",'F',"cheruvugowthami@gmail.com",""),
+(8, "18501A0570","Kavitha Mandala","",'F',"mkavithamandala@gmail.com",""),
+(9, "18501A05B8","Priyanka Vuyyuru","",'F',"vuyyurupriyanka27@gmail.com",""),
+(10,"18501A0568","Spandana Mallempati","",'F',"spandana.mallempati@gmail.com",""),
+(11, "18501A0506","Tonika Devi Avanigadda","",'F',"tonikadevi@gmail.com","");
