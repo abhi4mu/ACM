42335af8eb15082f55afd9defb080c24246f366b
ynvsb <83894314+ynvsb@users.noreply.github.com>
2021-06-02 17:25:46 +0530

    2016 members

---
+++ SQL/student_members_2012..sql
@@ -0,0 +1,42 @@
+/*2017 batch*/
+
+
+USE ACM;
+
+INSERT INTO STUDENT_MEMBERS VALUES 
+(44, " ","M.Nikhilesh"," ",'M',"Nikhilesh.murala@gmail.com"," "),
+(45, " ","B. Laxman"," ",'M',"laxmanbandaru@ymail.com"," "),
+(46, " ","M.Aparanjitha Reddy"," ",'M',"Aparanjitha.mummadi@gmail.com"," "),
+(47, " ","K. Sai Sri Ram"," ",'M',"saisriramkotcharlakota@gmail.com"," "),
+(48, " ","Ch. Chaitanya Varma"," ",'M',"chaitanya.varma.cse@gmail.com"," "),
+(49, " ","S.Sai Nikhila"," ",'F',"sainikhilak@gmail.com"," "),
+(50, " ","M.Nikhilesh"," ",'M',"Nikhilesh.murala@gmail.com"," "),
+(51, " ","K.Swathi"," ",'F',"Swathi_kompella@yahoo.com"," "),
+(52, " ","B.Naveen"," ",'M',"Smile.naveen@yahoo.com"," "),
+(53, " ","P.Srilakshmi"," ",'F',"sreelakshmipopuri@gmail.com"," "),
+(54, " ","A.chandu pavan"," ",'M',"annechandu@gmail.com"," "),
+(55, " ","Priyanka Bala G."," ",'F',"priyanka.bala@yahoo.com"," "),
+(56, " ","V.Divya"," ",'F',"diva4492@gmail.com"," "),
+(57, " ","Ch.V.S.Suryanarayana"," ",'M',"chundurisurya@yahoo.com"," "),
+(58, " ","B.Vijayasri"," ",'F',"vijaya.b81@gmail.com"," "),
+(59, " ","V.Sowmini"," ",'F',"sowmiveeramachaneni@gmail.com"," "),
+(60, " ","Karri priyanka"," ",'F',"karripriyanka@gmail.com"," "),
+(61, " ","Sreeja ketineni"," ",'F',"sreejakethineni@gmail.com"," "),
+(62, " ","Swetha Moparthy"," ",'F',"Swetha.moparthy@gmail.com"," "),
+(63, " ","Sai Sneha.velagapudi"," ",'F',"Dolly8.sneha@gmail.com"," "),
+(64, " ","Sri sagar Gorripati"," ",'M',"Srisagargorripati9@gmail.com"," "),
+(65, " ","Aquib Ahmed"," ",'M',"Aquib0507@gmail.com"," "),
+(66, " ","Anil kumar Vallabhaneni"," ",'M',"Anilv.90@gmail.com"," "),
+(67, " ","A.Satya Kalyan"," ",'M',"Satyakalyan_a@yahoo.com"," "),
+(68, " ","A.sudhir Babu"," ",'M',"asbabu@hotmail.com"," "),
+(69, " ","K.Dinesh"," ",'M',"Dinesh.kavuri@gmail.com"," "),
+(70, " ","G.V.Rajya Lakshmi"," ",'F',"gvrlaxmi@gmail.com"," "),
+(71, " ","K.Swetha"," ",'F',"Swetha_kolachana@yahoo.com"," "),
+(72, " ","M.Praveena"," ",'F',"Praveena_sanju25@yahoo.com"," "),
+(73, " ","P.Rukminidevi"," ",'F',"Rukmini04@gmail.co"," "),
+(74, " ","C.Manasa"," ",'F',"Manasa.chilamakuri@gmail.com"," "),
+(75, " ","K.Anitha"," ",'F',"Anu80koneru@gmail.com"," ");
+
+
+-- SELECT * FROM EVENT_NAMES ORDER BY START_DATE DESC;
+-- truncate EVENT_NAMES;
