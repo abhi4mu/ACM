061644a9dabcab70a121984f0edded3ecffaffc7
ynvsb <83894314+ynvsb@users.noreply.github.com>
2021-06-03 18:07:31 +0530

    updated faulty members and years(2020)

---
+++ SQL/faulty_members_2020.sql
@@ -0,0 +1,13 @@
+/* 2020 BATCH*/
+
+INSERT INTO FACULTY_MEMBERS VALUES
+(1," ","Jayalakshmi Aravapalli,",0,'F',"jayalakshmivallabhaneni@gmail.com"),
+(2," ","M V Ramakrishna",0,'M',"drmvrk@gmail.com"),
+(3," ","Ramadevi Jujjuri",0,'F',"k.ramakarthik@gmail.com"),
+(4," ","sri Lakshmi Talasila",0,'F',"tslakshmi@pvpsiddhartha.ac.in"),
+(5," ","Madhavi S",0,'F',"mmadhavi@pvpsiddhartha.ac.in"),
+(6," ","Venkata Nallagattla",0,'F',"nallagattla@gmail.com"),
+(7," ","Surekha Y",0,'F',"yalamanchili.surekha@gmail.com"),
+(8," ","Siva Parvathi Vallabhaneni",0,'F',"vallabhaneni.vsp@gmail.com"),
+(9," ","Lalitha Kumari Gaddala",0,'F',"lalithajoy.nuthakki@gmail.com"),
+(10," ","Sree Lakshmi Damineni",0,'F',"damineni.mtech@gmail.com");
